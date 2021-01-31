e1fa321c534c9a0bda85a7baadc84609f0ee5790
Nikita Denysenko <53399334+NikitaDenysenko@users.noreply.github.com>
2021-01-31 12:19:05 +0200

    remade gameover screen

--- screen/GameOverScreen.js
+++ screen/GameOverScreen.js
@@ -1,32 +1,85 @@
-import React, {useState} from 'react';
-import { StyleSheet, Text, View, Button, Image, Dimensions, ScrollView } from 'react-native';
+import React, { useState, useEffect } from "react";
+import {
+    StyleSheet,
+    Text,
+    View,
+    Button,
+    Image,
+    Dimensions,
+    ScrollView,
+} from "react-native";
 
-import BodyText from '../components/BodyText';
-import TitleText from '../components/TitleText';
-import Colors from '../constants/color';
-import MainButton from '../components/MainButton';
-import firebase from '../firebaseDB';
+import BodyText from "../components/BodyText";
+import TitleText from "../components/TitleText";
+import Colors from "../constants/color";
+import MainButton from "../components/MainButton";
+import firebase from "../firebaseDB";
 
 const GameOverScreen = (props) => {
-    const [dbRef, setDbRef] = useState(firebase.firestore().collection('scores'))
+    const [dbRef, setDbRef] = useState(
+        firebase.firestore().collection("scores")
+    );
+    const [estimationOfResult, setEstimationOfResult] = useState("");
+
+    const { roundsNumber } = props;
 
     const storeData = () => {
+        console.log(estimationOfResult);
         dbRef.add({
-            numberOfPoints: props.roundsNumber
-        })
-    }
+            numberOfPoints: roundsNumber,
+            estimation: estimationOfResult,
+        });
+    };
+
+    useEffect(() => {
+        // switch (roundsNumber) {
+        //     case roundsNumber >= 1 && roundsNumber <= 3:
+        //         return setEstimationOfResult("Perfect");
+
+        //     case roundsNumber >= 4 && roundsNumber <= 6:
+        //         return setEstimationOfResult("Good");
+
+        //     case roundsNumber >= 7 && roundsNumber <= 9:
+        //         return setEstimationOfResult("Not Bad");
+
+        //     case roundsNumber >= 10:
+        //         return setEstimationOfResult("Need Practice");
+        //     default:
+        //         break;
+        // }
+
+        if (roundsNumber >= 1 && roundsNumber <= 3) {
+            setEstimationOfResult("Perfect");
+        }
+        if (roundsNumber >= 4 && roundsNumber <= 6) {
+            setEstimationOfResult("Good");
+        }
+        if (roundsNumber >= 7 && roundsNumber <= 9) {
+            setEstimationOfResult("Not Bad");
+        }
+        if (roundsNumber >= 10) {
+            setEstimationOfResult("Need Practice");
+        }
+        console.log(estimationOfResult);
+    }, [roundsNumber, estimationOfResult, setEstimationOfResult]);
 
     return (
         <ScrollView>
             <View style={styles.screen}>
                 <TitleText>The Game is Over!</TitleText>
                 <View style={styles.imageContainer}>
-                    <Image source={require('../assets/success.png')} style={styles.image} />
+                    <Image
+                        source={require("../assets/success.png")}
+                        style={styles.image}
+                    />
                 </View>
                 <View style={styles.resultContainer}>
                     <BodyText style={styles.resultText}>
-                        Your phone needed <Text style={styles.highlight}>{props.roundsNumber}</Text>{' '}
-                        rounds to guess the number{' '}
+                        You needed{" "}
+                        <Text style={styles.highlight}>
+                            {props.roundsNumber}
+                        </Text>{" "}
+                        rounds to guess the number{" "}
                         <Text style={styles.highlight}>{props.userNumber}</Text>
                     </BodyText>
                 </View>
@@ -42,33 +95,33 @@ export default GameOverScreen;
 const styles = StyleSheet.create({
     screen: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        paddingVertical: 10
+        justifyContent: "center",
+        alignItems: "center",
+        paddingVertical: 10,
     },
     imageContainer: {
-        width: Dimensions.get('window').width * 0.7,
-        height: Dimensions.get('window').width * 0.7,
-        borderRadius: (Dimensions.get('window').width * 0.7) / 2,
+        width: Dimensions.get("window").width * 0.7,
+        height: Dimensions.get("window").width * 0.7,
+        borderRadius: (Dimensions.get("window").width * 0.7) / 2,
         borderWidth: 3,
-        borderColor: 'black',
-        overflow: 'hidden',
-        marginVertical: Dimensions.get('window').height / 30,
+        borderColor: "black",
+        overflow: "hidden",
+        marginVertical: Dimensions.get("window").height / 30,
     },
     image: {
-        width: '100%',
-        height: '100%',
+        width: "100%",
+        height: "100%",
     },
     resultContainer: {
         marginHorizontal: 30,
-        marginVertical: Dimensions.get('window').height / 60,
+        marginVertical: Dimensions.get("window").height / 60,
     },
     resultText: {
-        textAlign: 'center',
-        fontSize: Dimensions.get('window').height < 400 ? 16 : 20,
+        textAlign: "center",
+        fontSize: Dimensions.get("window").height < 400 ? 16 : 20,
     },
     highlight: {
         color: Colors.primary,
-        fontFamily: 'open-sans-bold',
+        fontFamily: "open-sans-bold",
     },
 });
